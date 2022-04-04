366b3186c822733dab79a8a6de7dbdf52ec51a52
Alex Visan <alexandruvisan1908@gmail.com>
2022-04-04 16:07:04 +0300

    new problem

---
+++ JS-code-problems/twice-as-old.js
@@ -0,0 +1,17 @@
+// Your function takes two arguments:
+
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+	if (sonYearsOld * 2 > dadYearsOld) {
+		return sonYearsOld * 2 - dadYearsOld;
+	} else if (sonYearsOld * 2 < dadYearsOld) {
+		return dadYearsOld - sonYearsOld * 2;
+	} else {
+		return 0;
+	}
+}
+
+console.log(twiceAsOld(42, 21));
--- main.js
+++ main.js
@@ -1,85 +1,26 @@
-// let deckId = "";
-// let scorePlayer1 = 0;
-// let scorePlayer2 = 0;
+// Triangular numbers are so called because of the equilateral triangular shape that they occupy when laid out as dots. i.e.
 
-// fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-// 	.then((res) => res.json())
-// 	.then((data) => {
-// 		deckId = data.deck_id;
-// 	})
-// 	.catch((err) => {
-// 		console.log(`error ${err}`);
-// 	});
+// 1st (1)   2nd (3)    3rd (6)
+// *          **        ***
+//            *         **
+//                      *
+// You need to return the nth triangular number. You should return 0 for out of range values:
 
-// document.querySelector("button").addEventListener("click", getFetch);
+// For example: (Input --> Output)
 
-// function returnValue(value) {
-// 	if (value === "ACE") {
-// 		return 14;
-// 	} else if (value === "KING") {
-// 		return 13;
-// 	} else if (value === "QUEEN") {
-// 		return 12;
-// 	} else if (value === "JACK") {
-// 		return 11;
-// 	} else {
-// 		return Number(value);
-// 	}
-// }
+// 0 --> 0
+// 2 --> 3
+// 3 --> 6
+// -10 --> 0
 
-// function checkWinnerRound(player1, player2) {
-// 	if (returnValue(player1) > returnValue(player2)) {
-// 		scorePlayer1 += 1;
-// 		document.querySelector(".player1score").innerHTML = scorePlayer1;
-// 		return "Player 1 WON!";
-// 	} else if (returnValue(player1) < returnValue(player2)) {
-// 		scorePlayer2 += 1;
-// 		document.querySelector(".player2score").innerHTML = scorePlayer2;
-// 		return "Player 2 WON!";
-// 	} else {
-// 		return "DRAW!";
-// 	}
-// }
-
-// function checkGameWinner(player1, player2) {
-// 	if (player1 > player2) {
-// 		return "PLAYER 1 WON THE GAME!";
-// 	} else if (player1 < player2) {
-// 		return "PLAYER 2 WON THE GAME!";
-// 	} else {
-// 		return "DRAW";
-// 	}
-// }
-
-// function getFetch() {
-// 	const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`;
-// 	fetch(url)
-// 		.then((res) => res.json())
-// 		.then((data) => {
-// 			console.log(data);
-// 			document.querySelector("#player1").src = data.cards[0].image;
-// 			document.querySelector("#player2").src = data.cards[1].image;
-
-// 			document.querySelector("h3").innerHTML = checkWinnerRound(data.cards[0].value, data.cards[1].value);
-// 			document.querySelector(".remaining").innerHTML = data.remaining;
-
-// 			if (data.remaining === 0) {
-// 				setTimeout(() => {
-// 					alert(checkGameWinner(scorePlayer1, scorePlayer2));
-// 				}, 1500);
-// 			}
-// 		})
-// 		.catch((err) => {
-// 			console.log(`error ${err}`);
-// 		});
-// }
-
-function duplicate(arr) {
-	let newArr = [...arr];
-	for (let i = 1; i <= arr.length; i++) {
-		newArr.push(i);
+// Return the nth triangular number
+function triangular(n) {
+	let tringArr = [];
+	if (Math.sign(n) == -1) return 0;
+	for (let i = 1; i <= n; i++) {
+		tringArr.push((i * (i + 1)) / 2);
 	}
-	return newArr;
+	return tringArr.reduce((acc, v) => acc + v);
 }
 
-console.log(duplicate([1, 2, 3, 4, 5]));
+console.log(triangular(2));
