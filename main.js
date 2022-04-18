0fdef054b1443eb264fc21ee008cc234e9c91ab6
Alex Visan <alexandruvisan1908@gmail.com>
2022-04-18 09:37:27 +0300

    changes

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-function evenNumbers(array, number) {
-	let num = number;
-	let newArr = array.filter((num) => num % 2 === 0);
-	let newArr2 = [];
-	for (let i = newArr.length - 1; num > 0; i--) {
-		newArr2.unshift(newArr[i]);
-		num--;
+function multiply(num1, num2) {
+	if (num2 !== undefined) {
+		return num1 + num2;
 	}
-	return newArr2;
+	return function executeMultiply(num2) {
+		return num1 * num2;
+	};
 }
 
-console.log(evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2));
+console.log(multiply(4, 5));
+const double = multiply(2);
+console.log(double(2));
