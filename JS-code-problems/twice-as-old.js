6e9203179ac6d37f811a87931f11f650bb562f15
Alex Visan <alexandruvisan1908@gmail.com>
2022-05-23 15:08:36 +0300

    change

--- JS-code-problems/twice-as-old.js
+++ JS-code-problems/twice-as-old.js
@@ -2,7 +2,6 @@
 // current father's age (years)
 // current age of his son (years)
 // Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
-
 function twiceAsOld(dadYearsOld, sonYearsOld) {
 	if (sonYearsOld * 2 > dadYearsOld) {
 		return sonYearsOld * 2 - dadYearsOld;
